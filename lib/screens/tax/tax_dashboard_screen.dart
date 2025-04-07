6e550c95686f9c278d80f87a58418f4cd74f3fcd
dsabugeorge9922 <darsansabu09@gmail.com>
2025-04-06 23:10:38 -0400

    updated the dashbord

--- lib/screens/tax/tax_dashboard_screen.dart
+++ lib/screens/tax/tax_dashboard_screen.dart
@@ -1,4 +1,3 @@
-// lib/screens/tax/tax_dashboard_screen.dart
 import 'package:finance_tracker/screens/tax/InvoiceScreen/invoice_screen.dart';
 import 'package:finance_tracker/screens/tax/tax_notifications_screen.dart';
 import 'package:finance_tracker/screens/tax/tax_upload_screen.dart';
@@ -11,53 +10,50 @@ class TaxDashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Tax Services')),
+      appBar: AppBar(
+        title: const Text('Tax Services'),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: 1.2,
           children: [
-            _buildCard(
+            _buildDashboardCard(
               context,
               icon: Icons.upload_file,
-              title: "Upload Tax Documents",
-              onTap:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => TaxUploadScreen()),
-                  ),
+              title: "Upload Documents",
+              color: Colors.blue.shade50,
+              iconColor: Colors.blue,
+              onTap: () => _navigateTo(context, TaxUploadScreen()),
             ),
-            const SizedBox(height: 16),
-            _buildCard(
+            _buildDashboardCard(
               context,
-              icon: Icons.calculate,
+              icon: Icons.calculate_outlined,
               title: "Tax Calculator",
-              onTap:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => TaxCalculatorScreen()),
-                  ),
+              color: Colors.green.shade50,
+              iconColor: Colors.green,
+              onTap: () => _navigateTo(context, TaxCalculatorScreen()),
             ),
-            const SizedBox(height: 16),
-            _buildCard(
+            _buildDashboardCard(
               context,
-              icon: Icons.notifications,
-              title: "Tax Notifications",
-              onTap:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => TaxNotificationsScreen()),
-                  ),
+              icon: Icons.notifications_active_outlined,
+              title: "Tax Alerts",
+              color: Colors.orange.shade50,
+              iconColor: Colors.orange,
+              onTap: () => _navigateTo(context, TaxNotificationsScreen()),
             ),
-            const SizedBox(height: 16),
-            _buildCard(
+            _buildDashboardCard(
               context,
-              icon: Icons.notifications,
-              title: "Invoice",
-              onTap:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => InvoiceScreen()),
-                  ),
+              icon: Icons.receipt_long_outlined,
+              title: "Invoice Generator",
+              color: Colors.purple.shade50,
+              iconColor: Colors.purple,
+              onTap: () => _navigateTo(context, InvoiceScreen()),
             ),
           ],
         ),
@@ -65,33 +61,55 @@ class TaxDashboardScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildCard(
+  void _navigateTo(BuildContext context, Widget screen) {
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => screen,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeTransition(opacity: animation, child: child);
+        },
+        transitionDuration: const Duration(milliseconds: 300),
+      ),
+    );
+  }
+
+  Widget _buildDashboardCard(
     BuildContext context, {
     required IconData icon,
     required String title,
+    required Color color,
+    required Color iconColor,
     required VoidCallback onTap,
   }) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 6,
-              offset: Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Row(
-          children: [
-            Icon(icon, size: 28, color: Colors.deepPurple),
-            const SizedBox(width: 16),
-            Text(title, style: const TextStyle(fontSize: 16)),
-          ],
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
+        onTap: onTap,
+        splashColor: color.withOpacity(0.3),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+                child: Icon(icon, size: 28, color: iconColor),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
